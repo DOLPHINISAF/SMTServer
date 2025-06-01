6e0c00291fac3131d44eb0d54f2e4c3293bb1b0c
Catalin Nedelescu <catalin.nedelescu@yahoo.com>
2025-06-01 22:44:34 +0300

    server now handles api calls

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+const mysql = require('mysql2')
+
 const { json } = require('stream/consumers');
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({port : 1337});
+
+const sqlConnection = mysql.createConnection({
+    host: '192.168.0.140',
+    user: 'SMTServer',
+    database: 'servermonitortool',
+});
+
 
 console.log("Started websocket!");
 
 const webClients = new Map();
 
 wss.on("connection", ws =>{
-    console.log("Connecteio");
+    console.log("User connected");
 
 
     clientAPI = "";
@@ -18,31 +27,23 @@ wss.on("connection", ws =>{
         console.log("New message");
 
         msgjson = JSON.parse(message);
-
+            if(msgjson.test == true && msgjson.source == "api"){
+                console.log("Api connected");
+            }
+        
         if(msgjson.type){
             if(msgjson.type === "auth"){
-                clientAPI = msgjson.APIKey;
-                webClients.set(clientAPI,ws);
-                console.log(msgjson);
-                setTimeout(()=>{
-                    msg={
-                        "type":"add",
-                        "dataID":"TEST!",
-                        "APIKey":"E2C820609140966AB26987D8839A7D6F8709A57B1788B427",
-                        "name":"TEST",
-                        "description":"TESTDESC",
-                        "value":"100",
-                        "status":"OK"
-                    };
-                    jsonmsg = JSON.stringify(msg);
-                    console.log(msg);
-                    ws.send(jsonmsg);    
-                    },5000);
+ 
+                if(HandleAuth(msgjson) == false){
+                    ws.close();
+                }
+    
             }
             else if(msgjson.type === "data"){
-                webClients[msgjson.APIKey].send(message);
+                //webClients[msgjson.APIKey].send(message);
             }
             else if(msgjson.type === "add"){
+                console.log("Received json to add param")
 
             }
         }
@@ -52,8 +53,41 @@ wss.on("connection", ws =>{
     });
     ws.on("close", ws =>{
         webClients.delete(clientAPI);
-        console.log("dis");
+        console.log("Lost Connection! Maybe disconnecteds");
     });
 
 
 });
+
+//For api auth, returns true for correct api, false otherwise
+//For user, returns true
+function HandleAuth(msgjson){
+               
+    ReceivedAPI = msgjson.APIKey;
+    console.log(msgjson);
+
+    if(msgjson.source == "client"){
+        webClients.set(ReceivedAPI,ws);
+        return true;
+    }
+    else if(msgjson.source == "api"){
+        console.log("Checking api in db");
+
+
+        query = `SELECT * FROM users WHERE api_key = \"${ReceivedAPI}\"`;
+        console.log(query);
+        sqlConnection.query(query,
+            function(err, results, fields){
+                if(results){
+                    console.log(`Found api in database: ${ReceivedAPI}`);
+                    return true;
+                }
+                else{
+                    console.log(`Did not find api in database: ${ReceivedAPI}`);
+                    return false;
+                }
+                    
+            }
+        )
+    }
+}
